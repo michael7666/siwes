0eb14f4e43160c8b9f75c802bf0c376493f3bc36
michael7666 <51081939+michael7666@users.noreply.github.com>
2019-12-17 10:10:32 +0100

    Nnadozie
    pls help me

---
+++ AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace siwes.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Dashborad()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public ActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
